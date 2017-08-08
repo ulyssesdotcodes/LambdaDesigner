224a9e6df7268e95f08dee6ad24cf31482fc37e0
Ulysses Popple <ulysses.popple@gmail.com>
2017-08-08 01:11:00 -0400

    Create a scratch for testing

---
+++ scratch.hs
@@ -0,0 +1,15 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+import LambdaDesigner.Op
+import LambdaDesigner.Lib
+
+import Prelude hiding (floor, mod)
+
+import Control.Lens
+import Data.IORef
+import Data.Matrix
+
+import qualified Data.ByteString.Char8 as BS
+
+go = do r <- newIORef mempty
+        run2 r [constC [float 0]] [constC [float 0]]
